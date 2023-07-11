3c031f2a134995f9535047e646355f8eeb48b72b
gstaciaki <gstaciaki99@gmail.com>
2023-07-11 13:05:40 -0300

    User CRUD Created

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -1,6 +1,6 @@
 class UserController < ApplicationController
   skip_before_action :verify_authenticity_token
-  before_action :set_user, only: [:show]
+  before_action :set_user, only: [:show, :update, :destroy]
 
   def all
     @users = User.all
@@ -8,22 +8,39 @@ class UserController < ApplicationController
   end
 
   def show
+    render json: @user, status: :ok
   end
 
   def new
     @user = User.new(user_params)
 
-    return render json: { error: 'Email already exists' }, status: :unprocessable_entity if User.exists?(email: @user.email)
+    return render json: { error: "Email already exists" }, status: :unprocessable_entity if User.exists?(email: @user.email)
 
     return render json: @user, status: :created if @user.save
 
     render json: @user.errors, status: :unprocessable_entity
   end
 
+  def update
+    return render json: @user if @user.update(user_params)
+
+    render json: @user.error, status: :bad_request
+  end
+
+  def destroy
+    return render json: { message: 'User deleted'} if @user.delete
+
+    render json: @user.erro, status: :bad_request
+  end
+
   private
 
   def set_user
-    @user = User.find(params[:hash_id])
+    begin
+      @user = User.find(params[:hash_id])
+    rescue => exception
+      render json: { error: "User doesn't exists" }
+    end
   end
 
   def user_params
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,7 @@ Rails.application.routes.draw do
   
   get '/users', to: 'user#all' 
   post '/users', to: 'user#new'
+  get '/users/:hash_id', to: 'user#show', as: :user
+  patch '/users/:hash_id', to: 'user#update'
+  delete '/users/:hash_id', to: 'user#destroy'
 end
